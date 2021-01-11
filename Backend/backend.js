b5270fe93635b5c15b1ef127836d08a8a6af2f17
Terrii <minimatt10@gmail.com>
2021-01-11 11:27:12 +1030

    Forgot to re-init

--- Backend/backend.js
+++ Backend/backend.js
@@ -33,7 +33,7 @@ let startupCheck = setInterval(async function Startup() {
   if(Database.checkDBConnection() && GlobalItemsHandler.checkGlobalItems() && ManifestHandler.checkManifestMounted()) {
     //Initialize the backend and start running!
     clearInterval(startupCheck);
-    //init();
+    init();
 
     //Testing Below
     //Test.getClanInfo();
