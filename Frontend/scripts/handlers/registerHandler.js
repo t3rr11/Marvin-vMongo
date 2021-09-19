ef01be8c893e427381048130bb5cbc6c9c3e7fa0
Terrii <minimatt10@gmail.com>
2021-09-19 12:46:16 +0930

    Fixed bug where registration wouldn't return any message when it errored

--- Frontend/scripts/handlers/registerHandler.js
+++ Frontend/scripts/handlers/registerHandler.js
@@ -72,32 +72,67 @@ async function Register(prefix, message, command, users, registeredUser) {
       });
 
       let searchResults = [...search1, ...search2];
+
       if(searchResults.length > 1) {
         let embed = new Discord.MessageEmbed().setColor(0x0099FF).setFooter(DiscordConfig.defaultFooter, DiscordConfig.defaultLogoURL).setTimestamp();
-        let usernames = [];
-        let ids = [];
-        let clans = [];
 
         embed.setAuthor("Too many results...");
         embed.setDescription(`If I was unable to find your account it may be because it's under a different name, sometimes bungie does that.\n\nHere is a list of possibilities though. To select one please re-register with the ID associated: \`${prefix}Register 4611686018*****\` \n\n**Make sure you select the right platform if you play on more than 1 platform**`);
 
-        for(let i in searchResults) {
-          usernames.push(`${ GetPlatformEmoji(searchResults[i].membershipType) } ${ searchResults[i].bungieGlobalDisplayName }#${ searchResults[i].bungieGlobalDisplayNameCode }`);
-          ids.push(searchResults[i].membershipId);
-          await RequestHandler.GetClanFromMbmID(searchResults[i].membershipType, searchResults[i].membershipId, async function GetClanFromMbmID(isError, data) {
-            if(!isError) {
-              if(data.Response.results.length > 0) { clans.push(data.Response.results[0].group.name); }
-              else { clans.push("-"); }
+        if(searchResults.length > 10) {
+          let chunkyResult = MakeItChunky(searchResults, Math.round(searchResults.length / 10));
+
+          for(let chunk of chunkyResult) {
+
+            let usernames = [];
+            let ids = [];
+            let clans = [];
+            
+            for(let i in chunk) {
+              usernames.push(`${ GetPlatformEmoji(chunk[i].membershipType) } ${ chunk[i].bungieGlobalDisplayName }#${ chunk[i].bungieGlobalDisplayNameCode }`);
+              ids.push(chunk[i].membershipId);
+              await RequestHandler.GetClanFromMbmID(chunk[i].membershipType, chunk[i].membershipId, async function GetClanFromMbmID(isError, data) {
+                if(!isError) {
+                  if(data.Response.results.length > 0) { clans.push(data.Response.results[0].group.name); }
+                  else { clans.push("-"); }
+                }
+                else { clans.push("-"); }
+              });
             }
-            else { clans.push("-"); }
-          });
+    
+            embed.addField("Username", usernames, true);
+            embed.addField("Clan", clans, true);
+            embed.addField("Bungie ID", ids, true);
+          }
+        }
+        else {
+          let usernames = [];
+          let ids = [];
+          let clans = [];
+
+          for(let i in searchResults) {
+            usernames.push(`${ GetPlatformEmoji(searchResults[i].membershipType) } ${ searchResults[i].bungieGlobalDisplayName }#${ searchResults[i].bungieGlobalDisplayNameCode }`);
+            ids.push(searchResults[i].membershipId);
+            await RequestHandler.GetClanFromMbmID(searchResults[i].membershipType, searchResults[i].membershipId, async function GetClanFromMbmID(isError, data) {
+              if(!isError) {
+                if(data.Response.results.length > 0) { clans.push(data.Response.results[0].group.name); }
+                else { clans.push("-"); }
+              }
+              else { clans.push("-"); }
+            });
+          }
+  
+          embed.addField("Username", usernames, true);
+          embed.addField("Clan", clans, true);
+          embed.addField("Bungie ID", ids, true);
         }
 
-        embed.addField("Username", usernames, true);
-        embed.addField("Clan", clans, true);
-        embed.addField("Bungie ID", ids, true);
-
-        message.channel.send({embed});
+        try {
+          message.channel.send({embed});
+        }
+        catch(err) {
+          message.channel.send(`So something went wrong and this command just didn't work. It dun broke. Please report using \`${prefix}request\``);
+        }
       }
       else if(searchResults.length === 0) { message.reply(`No users with that name found... Try this: \n\n1. Goto https://guardianstats.com and login there. \n2. Then if required choose a platform. \n3. If not then just click your name next to the setting wheel which will reveal your membershipId. \n4. Once you have copied that ID then just use the command like this \`${prefix}Register 1234567890\`.`); }
       else { FinishRegistration(message, command, users, registeredUser, searchResults[0]); }
@@ -127,6 +162,17 @@ function GetPlatformEmoji(membershipType) {
     case 5: { return "<:stadia:769837546024730634>" }
     default: { return membershipType }
   }
+}
+
+function MakeItChunky(arr, n) {
+  var chunkLength = Math.max(arr.length/n ,1);
+  var chunks = [];
+  for (var i = 0; i < n; i++) {
+    if(chunkLength*(i+1) <= arr.length) { 
+      chunks.push(arr.slice(chunkLength*i, chunkLength*(i+1))) 
+    };
+  }
+  return chunks; 
 }
 
 module.exports = Register;
